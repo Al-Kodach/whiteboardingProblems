8bfba7a5d6c39d7c4b8ada7d1e7751542e9926d4
Al-kodach <alkodach351@gmail.com>
2023-05-20 13:46:43 -0400

    logBetweenStepper

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 logBetween
 Define a function logBetween(lowNum, highNum) that will return an array from lowNum to highNum, inclusive. Inclusive means that the range includes lowNum and highNum.
 
+logBetweenStepper
+Write a function logBetweenStepper(min, max, step) that takes in three numbers as parameters. The function should return an array of numbers between min and max at step intervals.
--- whiteboardPratics.js
+++ whiteboardPratics.js
@@ -1,26 +1,26 @@
 
 
-function logBetween(ln, hn) {
+function logBetween(min, max) {
      /* 
         space complexity = O(n) 
-        size of array of memory is already allocated;
+        size of array or memory is already allocated;
         comparing this to arr.push(), where the program has to request 
-        for memory to store data, meaning at every push, 
+        for memory to store data, where at every push, 
         the program have to request a memory block to contain the current size,
     */;
     
-    // time complexity is O(1) when hn is below 60000 
+    // time complexity is O(1) when max is below 60000 3ms
      /* 
-        time complexity = O(n); when hn is 600000 
-        increase range is 4ms - 6ms
+        time complexity = O(1); 
+        trade space - time: for 1000000 // 12ms
+        
     */
-    let arr = new Array(hn);
-    
-    for (let i = ln; i <= hn; i++) {
-        let index = Math.abs(ln - i)
-        arr[index] = i;
-    }
+    let arr = new Array(max);
 
+    for (let i = min; i <= max; i++) {
+        let index = Math.abs(min - i)
+        arr[ index ] = i;
+    }
 
     return arr;
 }
@@ -31,7 +31,32 @@ s = new Date();
 logBetween(-1, 2);  // => [-1, 0, 1, 2]
 logBetween(14, 6);  // => []
 logBetween(4, 6);  // => [4, 5, 6]
-logBetween(0, 60000);
+logBetween(0, 1000000); // Runtime:  11ms
+e = new Date();
+console.log('\nlogBetween -> Runtime: ', e - s + 'ms\n');
+
+function logBetweenStepper(min, max, step) {
+    /* 
+       logBetweenStepper space complexity is higher compare to logBetween stepper,
+       due to number of variables, time complexity is affected O(n) 
+    */
+
+    let tradeSpaceForTime = new Array(max);
+
+    for (let i = min; i <= max; i+=step) {
+        let index = Math.abs(min - i) / step;
+        tradeSpaceForTime[index] = i;
+    }
+    return tradeSpaceForTime;
+}
+
+s = new Date();
+// logBetweenStepper(5, 9, 1) // => [5, 6, 7, 8, 9]
+// logBetweenStepper(-10, 15, 5) // => [-10, -5, 0, 5, 10, 15]
+logBetweenStepper(0, 1000000, 1); // Runtime:  29ms array.
+
 e = new Date();
 
-console.log(e - s + 'ms');
+console.log('logBetweenStepper => Runtime: ', e - s + 'ms\n');
+
+
